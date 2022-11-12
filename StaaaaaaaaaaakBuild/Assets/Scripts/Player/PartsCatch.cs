2d753e91342d737bf9e3fc491f87efd72295d243
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-11-12 13:24:18 +0900

    掴む処理完成

--- StaaaaaaaaaaakBuild/Assets/Scripts/Player/PartsCatch.cs
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Player/PartsCatch.cs
@@ -9,17 +9,22 @@ namespace StackBuild
         [SerializeField] private InputSender inputSender;
 
         [SerializeField] private float catchupPower = 400.0f;
-        [SerializeField] private float catchupRange = 15.0f;
+        [SerializeField] private Vector3 catchupOffsetPosition = new Vector3(0.0f, -5.0f, 0.0f);
 
         private void OnTriggerStay(Collider other)
         {
+            if (!other.CompareTag("Parts"))
+                return;
+
             if(inputSender.Catch.Value && other.TryGetComponent(out Rigidbody rb))
                 Catch(rb);
         }
 
         private void Catch(Rigidbody rb)
         {
-            var center = transform.position;
+            var parentPosition = transform.parent.position;
+
+            var center = parentPosition + catchupOffsetPosition;
             var sub = center - rb.transform.position;
 
             rb.AddForceAtPosition(sub * (catchupPower * Time.deltaTime), center, ForceMode.VelocityChange);
