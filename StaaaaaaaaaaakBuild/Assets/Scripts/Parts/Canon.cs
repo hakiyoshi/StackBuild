f3a71dcbf153f48e1790cf3e9989d67399602657
Ryouma-Kawashima <rrisland21@gmail.com>
2022-11-14 16:56:04 +0900

    Canonスクリプト作成

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Parts/Canon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace StackBuild
+{
+    public class Canon : MonoBehaviour
+    {
+        public float ShootPower;
+        public float ShootAngle;
+        [SerializeField] private Transform shootPosition;
+
+        private void Start()
+        {
+
+        }
+
+        private void OnDrawGizmos()
+        {
+
+        }
+    }
+}
---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Parts/CanonEditor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace StackBuild
+{
+    [CustomEditor(typeof(Canon))]
+    public class CanonEditor : Editor
+    {
+        private Canon instance;
+
+        private void OnEnable()
+        {
+            instance = (Canon)target;
+
+        }
+
+        private void OnSceneGUI()
+        {
+            var transform = instance.transform;
+
+            EditorGUI.BeginChangeCheck();
+
+            var powerSlider = Handles.Slider(
+                transform.position + Vector3.up * instance.ShootPower,
+                transform.up);
+            if (EditorGUI.EndChangeCheck())
+            {
+                instance.ShootPower = Vector3.Dot(transform.up, powerSlider);
+            }
+        }
+    }
+}
