5184b70b78224a0a80b518ab231dd96086831eb9
Ryouma-Kawashima <rrisland21@gmail.com>
2022-11-14 16:55:48 +0900

    Parts作った

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Parts/PartsData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace StackBuild
+{
+    [CreateAssetMenu(menuName = "StackBuild/Parts Data", fileName = "Parts Data")]
+    public class PartsData : ScriptableObject
+    {
+        public List<Parts> prefabs = new List<Parts>();
+    }
+}
