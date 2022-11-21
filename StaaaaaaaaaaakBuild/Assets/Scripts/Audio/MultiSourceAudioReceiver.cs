975c127b8930bc9fd1d686eeb202e69268462c42
i-yuuki <i-yuuki@users.noreply.github.com>
2022-11-21 15:17:41 +0900

    効果音システムを追加
    BGM鳴らすやつの同時にいっぱい鳴らせるバージョンをつくた

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Audio/MultiSourceAudioReceiver.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UniRx;
+
+namespace StackBuild.Audio
+{
+    public sealed class MultiSourceAudioReceiver : MonoBehaviour
+    {
+
+        [SerializeField] private AudioChannel channel;
+        [SerializeField] private AudioSource sourcePrefab;
+        [SerializeField] private int sourceCount;
+
+        private AudioSource[] sources;
+        private int sourceIdx;
+
+        private void Awake()
+        {
+            sources = new AudioSource[sourceCount];
+            for (int i = 0; i < sourceCount; i++)
+            {
+                sources[i] = Instantiate(sourcePrefab, transform);
+            }
+
+            sourceIdx = 0;
+
+            channel.OnRequest.Subscribe(cue =>
+            {
+                var source = sources[sourceIdx];
+                sourceIdx = (sourceIdx + 1) % sources.Length;
+                source.clip = cue.Clip;
+                source.Play();
+            });
+            channel.OnStopRequest.Subscribe(_ =>
+            {
+                foreach (var source in sources)
+                {
+                    source.Stop();
+                }
+            });
+        }
+
+    }
+}
