4d879d60e26eb0d4b2ef1d9c0bef9164f61de554
i-yuuki <i-yuuki@users.noreply.github.com>
2022-11-21 14:26:23 +0900

    BGMシステムを追加
    * 別プロジェクトからコピペして微調整したもの
    
    * まだ調整入るかも
    
    * サンプルPrefab (receiver系スクリプト) は常に読み込まれるシーンに
      置くといい感じになるよ

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Audio/AudioChannel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UniRx;
+
+namespace StackBuild.Audio
+{
+    [CreateAssetMenu(fileName = "New Audio Channel", menuName = "ScriptableObject/Audio/AudioChannel")]
+    public sealed class AudioChannel : ScriptableObject
+    {
+
+        private readonly Subject<AudioCue> onRequest = new();
+        private readonly Subject<Unit> onStopRequest = new();
+
+        public IObservable<AudioCue> OnRequest => onRequest;
+        public IObservable<Unit> OnStopRequest => onStopRequest;
+
+        public void Request(AudioCue cue)
+        {
+            onRequest.OnNext(cue);
+        }
+
+        public void RequestStop()
+        {
+            onStopRequest.OnNext(Unit.Default);
+        }
+
+    }
+}
