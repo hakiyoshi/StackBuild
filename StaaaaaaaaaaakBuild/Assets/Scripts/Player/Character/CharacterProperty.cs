a1f11d348f7274ad3b73768aaec98ad1d6ccf7cf
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-11-25 16:09:11 +0900

    移動方向にキャラを向く＆少し傾ける

--- StaaaaaaaaaaakBuild/Assets/Scripts/Player/Character/CharacterProperty.cs
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Player/Character/CharacterProperty.cs
@@ -11,16 +11,22 @@ namespace StackBuild
         [field: SerializeField] public Material EffectMaterial2 { get; private set; }
 
         [field: Header("Move")]
-        [field: SerializeField] public float Acceleration { get; private set; } = 1000.0f;
-        [field: SerializeField] public float Deceleration { get; private set; } = 0.99f;
-        [field: SerializeField] public float MaxSpeed { get; private set; } = 20.0f;
+        [field: SerializeField, Tooltip("加速度")] public float Acceleration { get; private set; } = 1000.0f;
+        [field: SerializeField, Tooltip("減速度")] public float Deceleration { get; private set; } = 0.99f;
+        [field: SerializeField, Tooltip("最大速度")] public float MaxSpeed { get; private set; } = 20.0f;
+
+        [field: Header("MoveAnimation")]
+        [field: SerializeField, Tooltip("移動時の傾く角度(Euler)")] public float SlopeAngle{ get; private set; } = 10.0f;
+        [field: SerializeField, Tooltip("傾く時間")] public float SlopeTime{ get; private set; } = 5.0f;
+        [field: SerializeField, Tooltip("正面を向く時間")] public float LookForwardTime{ get; private set; } = 5.0f;
 
         [field: Header("Catch")]
-        [field: SerializeField] public float CatchupPower { get; private set; } = 400.0f;
-        [field: SerializeField] public Vector3 CatchupOffsetPosition { get; private set; } = new Vector3(0.0f, -5.0f, 0.0f);
-        [field: SerializeField] public float CatchEffectAppearanceTime { get; private set; } = 0.2f;
-        [field: SerializeField] public float CatchEffectDisappearingTime { get; private set; } = 0.2f;
+        [field: SerializeField, Tooltip("掴む強さ")] public float CatchupPower { get; private set; } = 400.0f;
+        [field: SerializeField, Tooltip("プレイヤー座標から掴む座標の差")] public Vector3 CatchupOffsetPosition { get; private set; } = new Vector3(0.0f, -5.0f, 0.0f);
 
-        [field: SerializeField] public Vector3 CatchEffectMaxSizeOffset { get; private set; } = new Vector3(3.5f, 2.8f, 3.5f);
+        [field: Header("CatchAnimation")]
+        [field: SerializeField, Tooltip("掴むエフェクトが出る時間")] public float CatchEffectAppearanceTime { get; private set; } = 0.2f;
+        [field: SerializeField, Tooltip("掴むエフェクトが消える時間")] public float CatchEffectDisappearingTime { get; private set; } = 0.2f;
+        [field: SerializeField, Tooltip("基準サイズから見たエフェクトの最大サイズの差")] public Vector3 CatchEffectMaxSizeOffset { get; private set; } = new Vector3(3.5f, 2.8f, 3.5f);
     }
 }
--- StaaaaaaaaaaakBuild/Assets/Scripts/Player/PlayerMove.cs
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Player/PlayerMove.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace StackBuild
@@ -10,11 +12,30 @@ namespace StackBuild
         [SerializeField] private InputSender inputSender;
         [SerializeField] private PlayerProperty playerProperty;
 
+        private CharacterProperty property
+        {
+            get
+            {
+                return playerProperty.characterProperty;
+            }
+        }
+
+        private Quaternion targetLook;
+
         private Vector3 velocity = Vector3.zero;
+
+        private Quaternion startRotation = Quaternion.identity;
+
+        private void Start()
+        {
+            startRotation = transform.rotation;
+        }
 
         private void Update()
         {
             Move();
+            LookForward();
+            Slope();
         }
 
         void Move()
@@ -23,21 +44,44 @@ namespace StackBuild
 
             //移動
             if (Mathf.Abs(dir.x) <= 0.0f)
-                velocity.x *= playerProperty.characterProperty.Deceleration;
+                velocity.x *= property.Deceleration;
 
             if (Mathf.Abs(dir.z) <= 0.0f)
-                velocity.z *= playerProperty.characterProperty.Deceleration;
+                velocity.z *= property.Deceleration;
 
             if (dir.sqrMagnitude > 0.0f)
             {
-                velocity += dir * (playerProperty.characterProperty.Acceleration * Time.deltaTime);
+                velocity += dir * (property.Acceleration * Time.deltaTime);
             }
 
             if (velocity.sqrMagnitude >=
-                playerProperty.characterProperty.MaxSpeed * playerProperty.characterProperty.MaxSpeed)
-                velocity = velocity.normalized * playerProperty.characterProperty.MaxSpeed;
+                property.MaxSpeed * property.MaxSpeed)
+                velocity = velocity.normalized * property.MaxSpeed;
 
             transform.position += velocity * Time.deltaTime;
+        }
+
+        void LookForward()
+        {
+            if (Mathf.Abs(new Vector3(inputSender.Move.Value.x, 0.0f, inputSender.Move.Value.y).sqrMagnitude) > 0.0f)
+                targetLook = Quaternion.LookRotation(-velocity);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation,
+                targetLook,
+                property.LookForwardTime * Time.deltaTime);
+        }
+
+        void Slope()
+        {
+            var raito = velocity.sqrMagnitude /
+                        (property.MaxSpeed * property.MaxSpeed);
+
+            var rotation = transform.rotation;
+            var target = Quaternion.AngleAxis(property.SlopeAngle * raito, -transform.right) * rotation;
+
+            rotation = Quaternion.Lerp(rotation, target, property.SlopeTime * Time.deltaTime);
+            transform.rotation = rotation;
+
         }
     }
 }
