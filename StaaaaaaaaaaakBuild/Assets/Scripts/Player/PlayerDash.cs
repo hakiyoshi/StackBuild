300c6b4e85e09ab8aa24eb94716c4939ebcb1e42
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-11-26 13:09:33 +0900

    ダッシュクラス作成

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Player/PlayerDash.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace StackBuild
+{
+    public class PlayerDash : MonoBehaviour
+    {
+        [SerializeField] private InputSender inputSender;
+        
+
+        void Dash()
+        {
+
+        }
+    }
+}
