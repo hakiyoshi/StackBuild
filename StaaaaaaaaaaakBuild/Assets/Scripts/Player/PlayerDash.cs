1075434a39afa676b96e1d651f12435f1ab130e0
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-11-26 14:06:38 +0900

    ダッシュのコード完成

--- StaaaaaaaaaaakBuild/Assets/Scripts/Player/PlayerDash.cs
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Player/PlayerDash.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using DG.Tweening;
+using UniRx;
 using UnityEngine;
 
 namespace StackBuild
@@ -7,10 +9,38 @@ namespace StackBuild
     {
         [SerializeField] private InputSender inputSender;
 
+        [SerializeField] private float DashAcceleration = 10.0f;
+        [SerializeField] private float DashMaxSpeed = 10.0f;
+        [SerializeField] private float DashTimeToMaxSpeed = 0.5f;
+        [SerializeField] private float DashDeceleratingSpeed = 0.5f;
+
+        [SerializeField] private Ease DashEaseOfAcceleration = Ease.OutCirc;
+        [SerializeField] private Ease DashEaseOfDeceleration = Ease.OutCirc;
+
+
+        private Vector3 velocity;
+
+        private void Start()
+        {
+            inputSender.Dash.Subscribe(x =>
+            {
+                Dash();
+            }).AddTo(this);
+        }
 
         void Dash()
         {
+            var dir = new Vector3(inputSender.Move.Value.x, 0.0f, inputSender.Move.Value.y).normalized;
 
+            DOVirtual.Vector3(Vector3.zero, dir * DashMaxSpeed, DashTimeToMaxSpeed, value => {
+                transform.position += value;
+            }).SetEase(DashEaseOfAcceleration).
+                OnComplete(() => {
+                DOVirtual.Vector3(dir * DashMaxSpeed, Vector3.zero, DashDeceleratingSpeed, value =>
+                {
+                    transform.position += value;
+                }).SetEase(DashEaseOfDeceleration);
+            }).Play();
         }
     }
 }
