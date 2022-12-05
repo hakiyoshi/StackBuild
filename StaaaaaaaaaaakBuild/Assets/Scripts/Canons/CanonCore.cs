b5b5b6f3c30b387c4868931a8a21ad2ef69d3f59
Ryouma-Kawashima <rrisland21@gmail.com>
2022-12-06 00:12:28 +0900

    不要なasync関数をObservableに変更

--- StaaaaaaaaaaakBuild/Assets/Scripts/Canons/CanonCore.cs
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Canons/CanonCore.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UniRx;
+using UniRx.Triggers;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -10,39 +11,29 @@ namespace StackBuild
 {
     public class CanonCore : MonoBehaviour
     {
-        public float ShootPower;
-        public float ShootAngle;
-
+        [SerializeField] private float shootDuration = 0.07f;
+        [SerializeField] private float shootPower = 10f;
+        [SerializeField] private float shootAngle = 5f;
         [SerializeField] private Transform shootTarget;
 
         private Queue<PartsCore> queue = new Queue<PartsCore>();
 
         private void Start()
         {
-            ShootTimerAsync(this.GetCancellationTokenOnDestroy()).Forget();
-        }
 
-        private async UniTaskVoid ShootTimerAsync(CancellationToken token)
-        {
-            while (!token.IsCancellationRequested)
-            {
-                if (queue.TryDequeue(out var parts) &&
-                    parts.gameObject.TryGetComponent(out Rigidbody rb))
+            Observable.Interval(TimeSpan.FromSeconds(shootDuration))
+                .Where(_ => queue.Count > 0)
+                .Select(_ => queue.Dequeue().GetComponent<PartsPhysics>())
+                .Subscribe(partsPhysics =>
                 {
                     var shootPos = shootTarget.position;
                     var sub = shootPos - transform.position;
-                    var force = sub.normalized * ShootPower;
+                    var force = sub.normalized * shootPower;
 
-                    rb.velocity = Vector3.zero;
-                    rb.position = shootPos;
-
-                    parts.Show();
-
-                    rb.AddForce(force, ForceMode.VelocityChange);
-                }
-
-                await UniTask.Delay(TimeSpan.FromSeconds(0.07f), cancellationToken: token);
-            }
+                    partsPhysics.Teleport(shootPos);
+                    partsPhysics.PartsCore.Show();
+                    partsPhysics.AddForce(force, ForceMode.VelocityChange);
+                }).AddTo(this);
         }
 
         public void Enqueue(PartsCore parts)
