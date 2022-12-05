da37d93bc60a38ea64a0e296548fd81b2e3e7300
Ryouma-Kawashima <rrisland21@gmail.com>
2022-12-06 00:11:12 +0900

    poolをミニマルに

--- StaaaaaaaaaaakBuild/Assets/Scripts/Pools/PartsPool.cs
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Pools/PartsPool.cs
@@ -16,34 +16,27 @@ namespace StackBuild
 
         protected override PartsCore CreateInstance()
         {
-            // 生成時の処理
             return GameObject.Instantiate(prefab, parent, true).Spawn();
         }
 
         protected override void OnBeforeRent(PartsCore instance)
         {
-            // 貸出前の処理
-            base.OnBeforeRent(instance);
             instance.Hide();
         }
 
         protected override void OnBeforeReturn(PartsCore instance)
         {
-            // 返却前の処理
-            base.OnBeforeReturn(instance);
             instance.Hide();
         }
 
         protected override void OnClear(PartsCore instance)
         {
-            // 削除時の処理
             base.OnClear(instance);
             instance.Despawn();
         }
 
         protected override void Dispose(bool disposing)
         {
-            // 後処理
             base.Dispose(disposing);
         }
     }
