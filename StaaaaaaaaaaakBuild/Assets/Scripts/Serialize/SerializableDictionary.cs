a1c8934d66798999f2f2d6ef3d50f3076d222f17
Ryouma-Kawashima <rrisland21@gmail.com>
2022-12-07 02:08:48 +0900

    シリアライズ可能な辞書を追加

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Serialize/SerializableDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Security;
+using Unity.VisualScripting;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace StackBuild
+{
+    [Serializable]
+    public class SKeyValuePair<TKey, TValue>
+    {
+        public TKey Key;
+        public TValue Value;
+    }
+
+    [Serializable]
+    public class SDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+    {
+        [SerializeField] private List<SKeyValuePair<TKey, TValue>> dictToList;
+
+        public SDictionary()
+        {
+            dictToList = new();
+        }
+
+        public void OnBeforeSerialize()
+        {
+            this.Clear();
+        }
+
+        public void OnAfterDeserialize()
+        {
+            foreach (var data in dictToList)
+            {
+                this.Add(data.Key, data.Value);
+            }
+        }
+    }
+}
