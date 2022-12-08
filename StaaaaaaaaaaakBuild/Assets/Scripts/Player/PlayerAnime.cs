2780a3f6d41c1c411ddfb31a5e543434e13e0e67
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-12-08 12:40:42 +0900

    アニメ制御用プログラム作成

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Player/PlayerAnime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace StackBuild
+{
+    public class PlayerAnime : MonoBehaviour
+    {
+        [SerializeField] private ModelSetup modelSetup;
+
+        private Animator animator;
+
+        private void Start()
+        {
+            modelSetup.modelObject.TryGetComponent(out animator);
+        }
+    }
+}
