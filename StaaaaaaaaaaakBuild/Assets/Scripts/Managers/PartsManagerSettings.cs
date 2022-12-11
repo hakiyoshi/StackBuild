3868b04418e8763baa730cb57f8ba23b5b808219
Ryouma-Kawashima <rrisland21@gmail.com>
2022-12-12 00:54:04 +0900

    各種設定の初期値を仮入れ

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Managers/PartsManagerSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace StackBuild
+{
+    [System.Serializable]
+    public class PartsSpawnRule
+    {
+        public int threshould;
+        public int count;
+        public float minSeconds;
+        public float maxSeconds;
+    }
+
+    [CreateAssetMenu(menuName = "Scriptable Objects/Parts Manager Settings")]
+    public class PartsManagerSettings : ScriptableObject
+    {
+        [field: SerializeField] public List<PartsSpawnRule> SpawnRuleList { get; private set; } = new();
+    }
+}
