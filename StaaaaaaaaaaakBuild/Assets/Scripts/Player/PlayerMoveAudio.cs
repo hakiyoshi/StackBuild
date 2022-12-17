b8e2b880b65f6de9c286696fbf96231b0b428305
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-12-17 18:20:58 +0900

    移動時のサウンド追加

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Player/PlayerMoveAudio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DG.Tweening;
+using StackBuild.Audio;
+using UniRx;
+using UnityEngine;
+
+namespace StackBuild
+{
+    public class PlayerMoveAudio : MonoBehaviour
+    {
+        [SerializeField] private AudioCue cue;
+        [SerializeField] private AudioSource source;
+        private ModelSetup modelSetup;
+        private InputSender inputSender => modelSetup.inputSender;
+        private void Start()
+        {
+            //モデルセットアップを取得
+            transform.parent.TryGetComponent(out modelSetup);
+
+            //オーディオセット
+            source.clip = cue.Clip;
+            source.loop = cue.Loop;
+            source.volume = 0f;
+            source.Play();
+
+            inputSender.Move.sender.Subscribe(x =>
+            {
+                if (x.sqrMagnitude == 0.0f)
+                {
+                    source.DOFade(0.0f, 0.5f).SetEase(Ease.OutCubic);
+                }
+                else
+                {
+                    source.DOFade(1.0f, 0.5f).SetEase(Ease.OutCirc);
+                }
+            }).AddTo(this);
+        }
+    }
+}
