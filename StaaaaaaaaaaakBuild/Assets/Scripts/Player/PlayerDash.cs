17e4884dd4c4c33f5d9355acd3a1d82e08c2d2df
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-12-19 00:00:40 +0900

    ダッシュヒット時とダッシュ時のサウンド再生機能

--- StaaaaaaaaaaakBuild/Assets/Scripts/Player/PlayerDash.cs
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Player/PlayerDash.cs
@@ -13,6 +13,7 @@ namespace StackBuild
         [SerializeField] private InputSender inputSender;
         [SerializeField] private PlayerProperty playerProperty;
 
+        [SerializeField] private AudioCue attackHitAudioCue;
         [SerializeField] private AudioCue dashAudioCue;
         [SerializeField] private AudioSourcePool pool;
 
@@ -95,8 +96,8 @@ namespace StackBuild
                 DashEffect();
                 DashServerRpc();
 
-                var audio = pool.Rent(dashAudioCue);
-                audio.PlayAndReturnWhenStopped();
+                //ヒット時の処理
+                pool.Rent(dashAudioCue).PlayAndReturnWhenStopped();
             }).AddTo(this);
 
             playerProperty.HitDashAttack.Subscribe(x =>
@@ -110,6 +111,9 @@ namespace StackBuild
                 {
                     inputSender.Dash.isPause = false;
                 }).AddTo(this);
+
+                //ヒット音
+                pool.Rent(attackHitAudioCue).PlayAndReturnWhenStopped();
             }).AddTo(this);
         }
 
