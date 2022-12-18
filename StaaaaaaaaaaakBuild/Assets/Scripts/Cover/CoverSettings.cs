30d2e0220ed6393ff3b03c23078cf93f0eb99e5b
Ryouma-Kawashima <rrisland21@gmail.com>
2022-12-18 22:37:19 +0900

    Coverが同期して開閉する機能を追加

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Cover/CoverSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace StackBuild
+{
+    [CreateAssetMenu(menuName = "Scriptable Objects/Cover Settings")]
+    public class CoverSettings : ScriptableObject
+    {
+        [field: SerializeField] public float AppearanceTime { get; private set; } = 15.0f;
+        [field: SerializeField] public float IntervalTime { get; private set; } = 5.0f;
+    }
+}
