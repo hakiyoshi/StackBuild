05be3c77914f27eaf5e701f0574d75f9d11c907a
i-yuuki <i-yuuki@users.noreply.github.com>
2022-12-19 19:19:40 +0900

    結果発表のデザイン微調整

--- StaaaaaaaaaaakBuild/Assets/Scripts/UI/ResultsHeightDisplay.cs
+++ StaaaaaaaaaaakBuild/Assets/Scripts/UI/ResultsHeightDisplay.cs
@@ -34,7 +34,7 @@ namespace StackBuild.UI
         {
             winLoseText.text = "WIN";
             await DisplayWinLoseCommon()
-                .Join(winLoseText.rectTransform.DOScale(1, 0.375f).From(2).SetEase(Ease.OutCubic))
+                .Join(winLoseText.rectTransform.DOScale(1, 0.5f).From(3).SetEase(Ease.OutQuart))
                 .Join(winLoseText.DOColor(new Color(0.96f, 0.56f, 0.05f), 0.2f).SetEase((time, duration, _, _) =>
                     time < duration && time % 0.1f >= 0.05f ? 0 : 1))
                 .Join(loseBackground.DOFade(1, 0.3f).SetDelay(0.125f));
@@ -53,7 +53,7 @@ namespace StackBuild.UI
                 .Join(heightText.rectTransform.DOScale(0.3f, 0.25f).SetEase(Ease.InCubic))
                 .Append(winLoseText.DOFade(0, 0).From())
                 .Append(heightText.rectTransform.DOLocalMoveY(120, 0.375f).From(0).SetEase(Ease.OutCubic))
-                .Join(DOTween.To(() => winLoseText.characterSpacing, v => winLoseText.characterSpacing = v, 40, 0.5f).From().SetEase(Ease.OutCubic));
+                .Join(DOTween.To(() => winLoseText.characterSpacing, v => winLoseText.characterSpacing = v, 50, 0.5f).From().SetEase(Ease.OutQuart));
         }
 
     }
