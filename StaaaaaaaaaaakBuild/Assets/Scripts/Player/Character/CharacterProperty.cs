d316f5cd0b875255dfbe75529953bf3e3ca88c84
i-yuuki <i-yuuki@users.noreply.github.com>
2022-12-20 19:30:03 +0900

    キャラクターに企業名を追加＆HUDに表示

--- StaaaaaaaaaaakBuild/Assets/Scripts/Player/Character/CharacterProperty.cs
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Player/Character/CharacterProperty.cs
@@ -6,6 +6,7 @@ namespace StackBuild
     [CreateAssetMenu(menuName = "Game/CharacterProperty")]
     public class CharacterProperty : ScriptableObject
     {
+        [field: SerializeField] public string CompanyName { get; private set; }
         [field: SerializeField] public ModelProperty Model { get; private set; }
         [field: SerializeField] public MoveProperty Move { get; private set; }
         [field: SerializeField] public DashProperty Dash { get; private set; }
