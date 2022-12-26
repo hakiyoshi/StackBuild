501d3a76ad8464efedf90c34b7b2d3ed24d81179
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-12-26 14:56:04 +0900

    テストコード

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/MenuNetwork/MenuPlayerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace StackBuild.MenuNetwork
+{
+    public class MenuPlayerTest : NetworkBehaviour
+    {
+        [SerializeField] private PlayerPropertyOperator menuNetwork;
+
+        [SerializeField] private int playerIndex;
+        [SerializeField] private int characterType;
+
+        private void Update()
+        {
+            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                menuNetwork.ChangeSelectedCharacter(playerIndex, characterType);
+            }
+        }
+    }
+}
