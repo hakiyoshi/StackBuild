0ae7f22020d28b60c0ecdc067be6260a4e98ff30
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-12-30 00:26:52 +0900

    リザルトにシーン遷移機能追加

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/UI/Input/UIInput.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using StackBuild.Game;
+using UniRx;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace StackBuild.UI
+{
+    public class UIInput : MonoBehaviour
+    {
+        [SerializeField] private UIInputSender uiInputSender;
+        private PlayerInput playerInput;
+
+        private void Start()
+        {
+            TryGetComponent(out playerInput);
+
+            var ui = playerInput.actions.FindActionMap("UI");
+            InputFromEvent.ActionMapFromEventStarted(ui, "Select").Subscribe(_ => { uiInputSender.Select.Send(true); }).AddTo(this);
+            InputFromEvent.ActionMapFromEventCanceled(ui, "Select").Subscribe(_ => { uiInputSender.Select.Send(false); }).AddTo(this);
+
+            InputFromEvent.ActionMapFromEventStarted(ui, "Cancel").Subscribe(_ => { uiInputSender.Cancel.Send(true); }).AddTo(this);
+            InputFromEvent.ActionMapFromEventCanceled(ui, "Cancel").Subscribe(_ => { uiInputSender.Cancel.Send(false); }).AddTo(this);
+        }
+    }
+}
---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/UI/Input/UIInputSender.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace StackBuild.UI
+{
+    [CreateAssetMenu(menuName = "Input/UIInputSender")]
+    public class UIInputSender : ScriptableObject
+    {
+        public SenderProperty<bool> Select = new SenderProperty<bool>();
+        public SenderProperty<bool> Cancel = new SenderProperty<bool>();
+    }
+}
