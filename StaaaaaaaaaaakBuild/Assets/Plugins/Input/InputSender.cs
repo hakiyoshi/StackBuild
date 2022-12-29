0d2b675e4e34c98079f034f5b3a44ca4047b7da0
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-12-29 15:52:33 +0900

    SenderPropertyを分割

--- StaaaaaaaaaaakBuild/Assets/Plugins/Input/InputSender.cs
+++ StaaaaaaaaaaakBuild/Assets/Plugins/Input/InputSender.cs
@@ -8,33 +8,6 @@ using UnityEngine.InputSystem;
 [CreateAssetMenu(menuName = "InputSender")]
 public class InputSender : ScriptableObject
 {
-    public class SenderProperty<T>
-    {
-        public IReadOnlyReactiveProperty<T> sender => data;
-        private ReactiveProperty<T> data;
-        public bool isPause = false;
-
-        public T Value => data.Value;
-
-        public SenderProperty(T initialValue)
-        {
-            data = new ReactiveProperty<T>(initialValue);
-        }
-
-        public SenderProperty()
-        {
-            data = new ReactiveProperty<T>();
-        }
-
-        public void Send(T value)
-        {
-            if (isPause)
-                return;
-
-            data.Value = value;
-        }
-    }
-
     public SenderProperty<Vector2> Move = new SenderProperty<Vector2>(Vector2.zero);
     public SenderProperty<bool> Catch = new SenderProperty<bool>(false);
     public SenderProperty<bool> Dash = new SenderProperty<bool>(false);
