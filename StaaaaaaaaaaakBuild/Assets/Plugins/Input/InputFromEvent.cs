f5f956ba1ca6d6d11541408018bf492b2f79aa6a
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-12-30 00:24:19 +0900

    InputにUI追加

--- StaaaaaaaaaaakBuild/Assets/Plugins/Input/InputFromEvent.cs
+++ StaaaaaaaaaaakBuild/Assets/Plugins/Input/InputFromEvent.cs
@@ -11,6 +11,13 @@ public class InputFromEvent
             x => actionMap[actionName].started -= x);
     }
 
+    public static IObservable<InputAction.CallbackContext> ActionMapFromEventStarted(InputAction actionMap)
+    {
+        return Observable.FromEvent<InputAction.CallbackContext>(
+            x => actionMap.started += x,
+            x => actionMap.started -= x);
+    }
+
     public static IObservable<InputAction.CallbackContext> ActionMapFromEventPerformed(InputActionMap actionMap, string actionName)
     {
         return Observable.FromEvent<InputAction.CallbackContext>(
@@ -18,10 +25,24 @@ public class InputFromEvent
             x => actionMap[actionName].performed -= x);
     }
 
+    public static IObservable<InputAction.CallbackContext> ActionMapFromEventPerformed(InputAction actionMap)
+    {
+        return Observable.FromEvent<InputAction.CallbackContext>(
+            x => actionMap.performed += x,
+            x => actionMap.performed -= x);
+    }
+
     public static IObservable<InputAction.CallbackContext> ActionMapFromEventCanceled(InputActionMap actionMap, string actionName)
     {
         return Observable.FromEvent<InputAction.CallbackContext>(
             x => actionMap[actionName].canceled += x,
             x => actionMap[actionName].canceled -= x);
+    }
+
+    public static IObservable<InputAction.CallbackContext> ActionMapFromEventCanceled(InputAction actionMap)
+    {
+        return Observable.FromEvent<InputAction.CallbackContext>(
+            x => actionMap.canceled += x,
+            x => actionMap.canceled -= x);
     }
 }
