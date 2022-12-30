6388108c76b4a20b377577efc0118dc24c2a6518
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-12-30 14:44:55 +0900

    リザルト用のボタン作成

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/UI/Network/ResultButtonNetwork.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using NetworkSystem;
+using StackBuild.Game;
+using UniRx;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace StackBuild.UI.Network
+{
+    public class ResultButtonNetwork : SyncWaitingSystem, ISelectHandler, IDeselectHandler
+    {
+#if UNITY_EDITOR
+        [SerializeField] private SceneAsset sceneAsset;
+        private void OnValidate()
+        {
+            if (sceneAsset != null)
+                sceneName = sceneAsset.name;
+        }
+#endif
+        [SerializeField] private string sceneName = "";
+        [SerializeField] private UIInputSender uiInputSender;
+        [SerializeField] private float clickCooltime = 0.1f;//連打対策
+
+        private bool standby = false;
+        private bool isSelected = false;
+
+        private void Start()
+        {
+            uiInputSender.Select.sender.Skip(1).ThrottleFirst(TimeSpan.FromSeconds(clickCooltime)).Where(x => x).Subscribe(x =>
+            {
+                if (!isSelected)
+                    return;
+
+                //現在の状態で変える
+                if (!standby)
+                    ButtonDown();
+                else
+                    ButtonUp();
+
+                standby = !standby;
+            }).AddTo(this);
+        }
+
+        protected override void OnSendStandby(int numWaitingToSignal)
+        {
+            if (IsSpawned && !IsServer)
+                return;
+
+            if (numWaitingToSignal >= NetworkManager.ConnectedClientsIds.Count)
+            {
+                NetworkSystemSceneManager.LoadScene(sceneName);
+            }
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            isSelected = true;
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            isSelected = false;
+        }
+
+        void ButtonDown()
+        {
+            //ネットワーク処理
+            if(IsSpawned)
+            {
+                SendStandbyServerRpc();
+            }
+            else
+            {
+                //シーンロード処理
+                if (sceneName == "")
+                    return;
+
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+
+        void ButtonUp()
+        {
+            if (IsSpawned)
+                SendStandbyReleaseServerRpc();
+        }
+    }
+}
