041e33cbabaca133cde48858bebc78b6e7eca9fa
harukei <58187672+hakiyoshi@users.noreply.github.com>
2023-01-06 16:52:31 +0900

    移動制限追加

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Player/PlayerMovementRestricted.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace StackBuild
+{
+    public class PlayerMovementRestricted : MonoBehaviour
+    {
+        [SerializeField] private Vector3 firstPoint;
+        [SerializeField] private Vector3 secondPoint;
+        [SerializeField] private float radius;
+
+        private void LateUpdate()
+        {
+            var posi = transform.position;
+            var capsuleVec = secondPoint - firstPoint;
+            var fpVec = posi - firstPoint;
+            var distance = capsuleVec * (Vector3.Dot(capsuleVec.normalized, fpVec) / capsuleVec.magnitude) - fpVec;
+
+            if (Vector3.Dot(fpVec.normalized, capsuleVec.normalized) <= 0.0f)
+            {
+                distance = firstPoint - posi;
+            }
+            else if(Vector3.Dot((posi - secondPoint).normalized, -capsuleVec.normalized) <= 0.0f)
+            {
+                distance = secondPoint - posi;
+            }
+
+            if (distance.sqrMagnitude > radius * radius)//半径より遠い場合
+            {
+                posi = (posi + distance) - distance.normalized * radius;
+            }
+            transform.position = posi;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(firstPoint, radius);
+            Gizmos.DrawWireSphere(secondPoint, radius);
+            Gizmos.DrawLine(firstPoint + new Vector3(0.0f, 0.0f, radius), secondPoint + new Vector3(0.0f, 0.0f, radius));
+            Gizmos.DrawLine(firstPoint - new Vector3(0.0f, 0.0f, radius), secondPoint - new Vector3(0.0f, 0.0f, radius));
+        }
+    }
+}
