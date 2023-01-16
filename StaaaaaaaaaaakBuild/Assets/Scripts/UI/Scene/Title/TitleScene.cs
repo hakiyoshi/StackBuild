e9bf85008ff290709a25be63dee8e3092b1839f6
i-yuuki <i-yuuki@users.noreply.github.com>
2023-01-16 17:29:53 +0900

    タイトル画面のクラスを分離・整理

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/UI/Scene/Title/TitleScene.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StackBuild.UI.Scene.Title
+{
+    public class TitleScene : MonoBehaviour
+    {
+
+        private const float MenuBackgroundAlpha = 0.5f;
+
+        internal const float SlideAccelerationDuration = 0.35f;
+        internal const Ease SlideAccelerationEasing = Ease.InCubic;
+        internal const float SlideDecelerationDuration = 0.75f;
+        internal const Ease SlideDecelerationEasing = Ease.OutQuint;
+
+        [SerializeField] private Image menuBackground;
+        [SerializeField] private TitleLogo logo;
+        [SerializeField] private TitleScreen titleScreen;
+
+        private void Start()
+        {
+            ShowTitleAsync().Forget();
+        }
+
+        private async UniTaskVoid ShowTitleAsync()
+        {
+            var logoTransform = (RectTransform)logo.transform;
+            var seq = DOTween.Sequence()
+                .Append(logoTransform.DOAnchorMin(new Vector2(0.5f, 0.5f), SlideAccelerationDuration).From().SetEase(SlideAccelerationEasing))
+                .Join(logoTransform.DOAnchorMax(new Vector2(0.5f, 0.5f), SlideAccelerationDuration).From().SetEase(SlideAccelerationEasing))
+                .Join(logoTransform.DOPivotX(0.5f, SlideAccelerationDuration).From().SetEase(SlideAccelerationEasing))
+                .Join(logoTransform.DOAnchorPosX(0, SlideAccelerationDuration).From().SetEase(SlideAccelerationEasing))
+                .Pause();
+
+            await logo.DisplayAsync();
+
+            await seq.Play().AsyncWaitForCompletion();
+            ShowBackground();
+            titleScreen.ShowAsync().Forget();
+        }
+
+        private void ShowBackground()
+        {
+            menuBackground.DOFade(MenuBackgroundAlpha, SlideDecelerationDuration).SetEase(Ease.OutQuad);
+            menuBackground.rectTransform.DOAnchorMin(new Vector2(0, 0), SlideDecelerationDuration).From().SetEase(SlideDecelerationEasing);
+        }
+
+    }
+}
--- StaaaaaaaaaaakBuild/Assets/Scripts/UI/Scene/Title/TitleScreen.cs
+++ StaaaaaaaaaaakBuild/Assets/Scripts/UI/Scene/Title/TitleScreen.cs
@@ -3,101 +3,67 @@ using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
 
 namespace StackBuild.UI.Scene.Title
 {
+    [RequireComponent(typeof(CanvasGroup))]
     public class TitleScreen : MonoBehaviour
     {
 
-        [SerializeField] private RectTransform logoContainer;
-        [SerializeField] private RectTransform iconContainer;
-        [SerializeField] private Image[] iconParts;
-        [SerializeField] private RectTransform titleMask;
-        [SerializeField] private Image menuBackground;
-        [SerializeField] private TMP_Text[] menuTexts;
-        [FormerlySerializedAs("flashText")] [SerializeField] private TMP_Text playText;
+        [SerializeField] private CanvasGroup container;
+        [SerializeField] private TMP_Text[] slideTexts;
+        [SerializeField] private TMP_Text playText;
         [SerializeField] private TMP_Text copyrightText;
 
-        private Sequence sequence;
+        private Tween playTextFlash;
+
+        private void Reset()
+        {
+            container = GetComponent<CanvasGroup>();
+        }
 
         private void Awake()
         {
-            sequence = DOTween.Sequence()
-                    .Append(ShowLogo())
-                    .Append(ShowTexts(0.35f, Ease.InCubic, 0.07f, 0.75f, Ease.OutQuint))
-                ;
-            sequence.SetAutoKill(false);
-            sequence.Pause();
+            container.alpha = 0;
+            container.interactable = false;
+
+            playTextFlash = playText.DOFade(0, 1).From(1).SetEase(Ease.InQuad).SetLoops(-1, LoopType.Yoyo).Pause();
         }
 
-        private void Start()
+        public async UniTask ShowAsync()
         {
-            DisplayAsync().Forget();
+            container.DOKill();
+            container.alpha = 1;
+            container.interactable = true;
+
+            playTextFlash.Rewind();
+            await ShowTexts(0.07f);
+            _ = playTextFlash.Play();
         }
 
-        private async UniTaskVoid DisplayAsync()
+        public async UniTask HideAsync()
         {
-            sequence.Restart();
-            await sequence.AsyncWaitForCompletion();
-            FlashText();
+            container.interactable = false;
+            await container.DOFade(0, 0.2f);
         }
 
-        private Sequence ShowLogo()
-        {
-            var seq = DOTween.Sequence();
-
-            foreach (var part in iconParts)
-            {
-                seq = seq
-                    .Join(part.DOFade(1, 0).From(0))
-                    .Join(part.rectTransform.DOLocalMoveY(30, 0.15f).From().SetDelay(0.07f).SetEase(Ease.InQuad));
-            }
-
-            seq = seq
-                .Append(iconContainer.DOAnchorPosX(0, 0.65f).From().SetEase(Ease.InOutQuart))
-                .Join(DOTween
-                    .To(() => titleMask.sizeDelta.x,
-                        x => titleMask.sizeDelta = new Vector2(x, titleMask.sizeDelta.y), 0, 0.65f).From()
-                    .SetEase(Ease.InOutQuart));
-
-            return seq;
-        }
-
-        private Sequence ShowTexts(float accelerationDuration, Ease accelerationEase,
-            float decelerationStagger, float decelerationDuration, Ease decelerationEase)
+        private Sequence ShowTexts(float stagger)
         {
             var seq = DOTween.Sequence();
-
-            seq = seq
-                .Append(logoContainer.DOAnchorMin(new Vector2(0.5f, 0.5f), accelerationDuration).From().SetEase(accelerationEase))
-                .Join(logoContainer.DOAnchorMax(new Vector2(0.5f, 0.5f), accelerationDuration).From().SetEase(accelerationEase))
-                .Join(logoContainer.DOPivotX(0.5f, accelerationDuration).From().SetEase(accelerationEase))
-                .Join(logoContainer.DOAnchorPosX(0, accelerationDuration).From().SetEase(accelerationEase));
-
-            seq = seq
-                .Append(menuBackground.DOFade(0, decelerationDuration).From().SetEase(Ease.OutQuad))
-                .Join(menuBackground.rectTransform.DOAnchorMin(new Vector2(0, 0), decelerationDuration).From().SetEase(decelerationEase));
 
             int i = 0;
-            foreach (var text in menuTexts)
+            foreach (var text in slideTexts)
             {
                 seq = seq
-                    .Join(text.DOFade(1, 0).From(0).SetDelay(i > 0 ? decelerationStagger : 0).SetEase(decelerationEase))
-                    .Join(text.rectTransform.DOAnchorPosX(-200, decelerationDuration).From(true)
-                        .SetEase(decelerationEase));
+                    .Join(text.DOFade(1, 0).From(0).SetDelay(i > 0 ? stagger : 0))
+                    .Join(text.rectTransform.DOAnchorPosX(-200, TitleScene.SlideDecelerationDuration).From(true)
+                        .SetEase(TitleScene.SlideDecelerationEasing));
                 i++;
             }
 
             seq = seq.Join(copyrightText.DOFade(0, 1).From());
 
             return seq;
-        }
-
-        private void FlashText()
-        {
-            playText.DOFade(0, 1).From(1).SetEase(Ease.InQuad).SetLoops(-1, LoopType.Yoyo);
         }
 
     }
