cad6943753a432249f9b69005b65bb9a47e00832
i-yuuki <i-yuuki@users.noreply.github.com>
2023-01-16 18:05:34 +0900

    タイトル画面の文字出すやつ別クラスに

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/UI/Scene/Title/TitleMenuStaggerDisplay.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StackBuild.UI.Scene.Title
+{
+    public class TitleMenuStaggerDisplay : MonoBehaviour
+    {
+
+        private const float DefaultStagger = 0.07f;
+
+        [SerializeField] private bool hideOnAwake;
+        [SerializeField] private Graphic[] items;
+
+        private void Awake()
+        {
+            if (hideOnAwake)
+            {
+                Hide();
+            }
+        }
+
+        public Sequence Display()
+        {
+
+            var seq = DOTween.Sequence();
+
+            int i = 0;
+            foreach (var item in items)
+            {
+                seq
+                    .Join(item.DOFade(1, 0).From(0).SetDelay(i > 0 ? DefaultStagger : 0))
+                    .Join(((RectTransform)item.transform).DOAnchorPosX(-200, TitleScene.SlideDecelerationDuration).From(true)
+                        .SetEase(TitleScene.SlideDecelerationEasing));
+                i++;
+            }
+
+            return seq;
+        }
+
+        public void Hide()
+        {
+            foreach (var item in items)
+            {
+                item.DOKill();
+                var color = item.color;
+                color.a = 0;
+                item.color = color;
+            }
+        }
+
+    }
+}
--- StaaaaaaaaaaakBuild/Assets/Scripts/UI/Scene/Title/TitleScreen.cs
+++ StaaaaaaaaaaakBuild/Assets/Scripts/UI/Scene/Title/TitleScreen.cs
@@ -11,11 +11,8 @@ namespace StackBuild.UI.Scene.Title
     {
 
         [SerializeField] private CanvasGroup container;
-        [SerializeField] private TMP_Text[] slideTexts;
+        [SerializeField] private TitleMenuStaggerDisplay staggerDisplay;
         [SerializeField] private TMP_Text playText;
-        [SerializeField] private TMP_Text copyrightText;
-
-        private Tween playTextFlash;
 
         private void Reset()
         {
@@ -24,46 +21,23 @@ namespace StackBuild.UI.Scene.Title
 
         private void Awake()
         {
-            container.alpha = 0;
             container.interactable = false;
-
-            playTextFlash = playText.DOFade(0, 1).From(1).SetEase(Ease.InQuad).SetLoops(-1, LoopType.Yoyo).Pause();
         }
 
         public async UniTask ShowAsync()
         {
             container.DOKill();
-            container.alpha = 1;
             container.interactable = true;
 
-            playTextFlash.Rewind();
-            await ShowTexts(0.07f);
-            _ = playTextFlash.Play();
+            await staggerDisplay.Display();
+            _ = playText.DOFade(0, 1).From(1).SetEase(Ease.InQuad).SetLoops(-1, LoopType.Yoyo);
         }
 
-        public async UniTask HideAsync()
+        public void Hide()
         {
             container.interactable = false;
-            await container.DOFade(0, 0.2f);
-        }
-
-        private Sequence ShowTexts(float stagger)
-        {
-            var seq = DOTween.Sequence();
-
-            int i = 0;
-            foreach (var text in slideTexts)
-            {
-                seq = seq
-                    .Join(text.DOFade(1, 0).From(0).SetDelay(i > 0 ? stagger : 0))
-                    .Join(text.rectTransform.DOAnchorPosX(-200, TitleScene.SlideDecelerationDuration).From(true)
-                        .SetEase(TitleScene.SlideDecelerationEasing));
-                i++;
-            }
-
-            seq = seq.Join(copyrightText.DOFade(0, 1).From());
-
-            return seq;
+            staggerDisplay.Hide();
+            playText.DOKill();
         }
 
     }
