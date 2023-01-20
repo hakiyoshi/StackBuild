b78414a2746ea57cbea8a21d9a7ab84206ec927e
i-yuuki <i-yuuki@users.noreply.github.com>
2023-01-20 16:32:35 +0900

    キャラクターに名前を追加

--- StaaaaaaaaaaakBuild/Assets/Scripts/Player/Character/CharacterProperty.cs
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Player/Character/CharacterProperty.cs
@@ -6,6 +6,7 @@ namespace StackBuild
     [CreateAssetMenu(menuName = "Game/CharacterProperty")]
     public class CharacterProperty : ScriptableObject
     {
+        [field: SerializeField] public string Name { get; private set; }
         [field: SerializeField] public string CompanyName { get; private set; }
         [field: SerializeField] public ModelProperty Model { get; private set; }
         [field: SerializeField] public Sprite Sprite { get; private set; }
