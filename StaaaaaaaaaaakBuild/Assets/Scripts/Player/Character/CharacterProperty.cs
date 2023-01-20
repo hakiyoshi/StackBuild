93c9f926dfe4e41e50d0b6e4491b1e0ccb83878d
i-yuuki <i-yuuki@users.noreply.github.com>
2023-01-20 17:21:19 +0900

    キャラクターにダッシュの説明を追加 ここでええんかな

--- StaaaaaaaaaaakBuild/Assets/Scripts/Player/Character/CharacterProperty.cs
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Player/Character/CharacterProperty.cs
@@ -8,6 +8,8 @@ namespace StackBuild
     {
         [field: SerializeField] public string Name { get; private set; }
         [field: SerializeField] public string CompanyName { get; private set; }
+        [field: TextArea]
+        [field: SerializeField] public string DashDescription { get; private set; }
         [field: SerializeField] public ModelProperty Model { get; private set; }
         [field: SerializeField] public Sprite Sprite { get; private set; }
         [field: SerializeField] public MoveProperty Move { get; private set; }
