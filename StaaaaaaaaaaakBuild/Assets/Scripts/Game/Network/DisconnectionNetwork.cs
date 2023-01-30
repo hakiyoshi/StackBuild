522356186b639d6f2fa0ea461ee96dfada2ea384
harukei <58187672+hakiyoshi@users.noreply.github.com>
2023-01-30 15:41:25 +0900

    ネットワークが落ちたらシーンをメインメニューに移動する

---
+++ StaaaaaaaaaaakBuild/Assets/Scripts/Game/Network/DisconnectionNetwork.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using NetworkSystem;
+using StackBuild.Scene.Title;
+using StackBuild.UI;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace StackBuild.Game
+{
+    public class DisconnectionNetwork : MonoBehaviour
+    {
+        [SerializeField] private RelayManager relay;
+        [SerializeField] private LobbyManager lobby;
+
+        private bool startNetwork = false;
+        private bool sceneChangeNow = false;
+
+        private void Start()
+        {
+            startNetwork = NetworkManager.Singleton != null && NetworkManager.Singleton.IsClient;
+        }
+
+        private void Update()
+        {
+            if (!sceneChangeNow && startNetwork && lobby.Status == LobbyManager.LobbyStatus.NonPerticipation)
+            {
+                sceneChangeNow = true;
+                LoadMainMenu(false);
+            }
+        }
+
+        private void LoadMainMenu(bool characterSelect)
+        {
+            TitleScene.MarkTitleSkip();
+            if (characterSelect)
+            {
+                TitleScene.MarkMainMenuSkip();
+            }
+            ChangeScene("MainMenu").Forget();
+        }
+
+        private async UniTask ChangeScene(string sceneName)
+        {
+            await NetworkSystemManager.NetworkExit(lobby, relay);
+            await UniTask.WaitWhile(() => NetworkManager.Singleton.ShutdownInProgress);
+            await LoadingScreen.Instance.ShowAsync();
+            await SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+}
